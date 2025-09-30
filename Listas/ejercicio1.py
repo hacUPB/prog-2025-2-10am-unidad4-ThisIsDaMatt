f6f78e484a90899d9cc05bdfe9292f32a5870a86
Matias Munera <matiasmunera08@gmail.com>
2025-09-30 11:06:53 -0500

    Se crearon archivos para la actividad

--- Listas/ejercicio1.py
+++ Listas/ejercicio1.py
@@ -1,11 +1,28 @@
+import random
 # Lista Vacia
 componentes = []
-
+# 1. Agregar un elemento
+elemento = input("Ingrese un componente del Avión: ")
+componentes.append(elemento)
+print(componentes)
+# Lista numerica
+numeros = []
+aleatorio = random.randint(0, 100)
+numeros.append(aleatorio)
+numeros.append(10)
+# Agregar numeros aleatorios a la lista
+for i in range(10):
+    aleatorio = random.randint(0, 100)
+    numeros.append(aleatorio)
+    numeros.append(10)
+print(numeros)
 # Lista con elementos
-componentes = ["alas", "fuselaje", "motores", "tren de aterrizaje"]
+# componentes = ["alas", "fuselaje", "motores", "tren de aterrizaje"]
 
 # Lista con diferentes tipos de datos
-datos_vuelo = [202, "Boeing 737", True, 10500.5]
+# datos_vuelo = [202, "Boeing 737", True, 10500.5]
 
 # Listas anidadas
-matriz_rotacion = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+# matriz_rotacion = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+
