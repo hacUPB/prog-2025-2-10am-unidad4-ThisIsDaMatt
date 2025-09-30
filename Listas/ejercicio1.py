f0536f7c9ead395655ee1f54b3edc011d45ccdf4
Matias Munera <matiasmunera08@gmail.com>
2025-09-30 10:48:26 -0500

    Prueba de Commit desde VSCode

---
+++ Listas/ejercicio1.py
@@ -0,0 +1,11 @@
+# Lista Vacia
+componentes = []
+
+# Lista con elementos
+componentes = ["alas", "fuselaje", "motores", "tren de aterrizaje"]
+
+# Lista con diferentes tipos de datos
+datos_vuelo = [202, "Boeing 737", True, 10500.5]
+
+# Listas anidadas
+matriz_rotacion = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
